289a416156dd543fe77302db083e17420b56fc5f
yuliayakupova@gmail.com <BarsukTvojDrug368>
2022-03-06 15:41:00 +0200

    initial commit

---
+++ src/main/java/com/example/MedPortal/MedPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedPortalApplication.class, args);
+	}
+
+}
